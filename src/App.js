7ce2327614cb0aa097ebcd83fe48c477a003ee9b
Ardani <sulistyoardani@gmail.com>
2020-03-25 20:04:20 +0700

    tes

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.module.css';
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
+import {PrivateRoute} from './support/helpers/privateRoute';
+// import PrivateRoute from './support/helpers/privateRoute';
+import Login from './c_login/tes';
+import Home from './c_home/home';
+import Layout from './c_home/layout';
+// import MaterialIcon from 'react-material-iconic-font'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/home" component={Home} />
+        {/* <Route path="/(login|)" component={Login} /> */}
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
