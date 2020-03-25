33e31b64abe18c0d99fab693f8b91a61baf7126a
Ardani <sulistyoardani@gmail.com>
2020-03-25 17:40:59 +0700

    Updates

---
+++ precache-manifest.756d57733b5c1742ae56fd83b88b72f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce614b1df19e21acf016d09bb5d461f",
+    "url": "/reactjs-skeleton/index.html"
+  },
+  {
+    "revision": "eab7e045009c8b77adde",
+    "url": "/reactjs-skeleton/static/css/2.a3b1703b.chunk.css"
+  },
+  {
+    "revision": "b4df341c956283d28523",
+    "url": "/reactjs-skeleton/static/css/main.c61914fb.chunk.css"
+  },
+  {
+    "revision": "eab7e045009c8b77adde",
+    "url": "/reactjs-skeleton/static/js/2.f690d741.chunk.js"
+  },
+  {
+    "revision": "b4df341c956283d28523",
+    "url": "/reactjs-skeleton/static/js/main.cd28c687.chunk.js"
+  },
+  {
+    "revision": "2beb191899e8b2c74e6c",
+    "url": "/reactjs-skeleton/static/js/runtime~main.aece5ade.js"
+  }
+]);
